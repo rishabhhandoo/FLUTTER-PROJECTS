847d10571beecf64cdeb1707de64b7314b7a2dea
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-07-19 23:00:42 +0530

    flash_chat_firebase

---
+++ flash_chat/lib/screens/welcome_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  // with TickerProviderStateMixin in case where more than one animation
+  //mixins provide a lot of functionality and alot of mixins can be added
+  AnimationController? controller;
+  Animation<double>? animation;
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync:
+          this, // aka this welcomeScreenState object is going to provide the ticker // this is used to reference the current class object
+      duration: Duration(seconds: 3),
+      //upperBound: 100.0, //removed when adding curves as they take value from 0-1
+    );
+    // animation = ColorTween(begin: Colors.red , end: Colors.blue).animate(controller!)as Animation<double>?;
+    controller?.addListener(() {
+      setState(
+          () {}); // this set state is added to justify that there will be changes
+      print(animation?.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'], // it requires list text
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
